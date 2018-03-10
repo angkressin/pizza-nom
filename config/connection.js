fdeece6f5bd9b72d4be00007992a0ba0ce1dee9e
Angela Kressin <angelakressin@Angelas-MacBook-Pro.local>
2018-03-09 23:56:57 -0500

    CRUD working and fixed dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,11 @@
 var mysql = require("mysql");
+require("dotenv").config();
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  // host: process.env.DB_HOST,
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: "pizza_db"
 });
 
