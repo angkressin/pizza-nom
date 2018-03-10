fe2152c8e0d2ce32b1dfcfcb6e06c74179b98be7
Angela Kressin <angelakressin@Angelas-MacBook-Pro.local>
2018-03-10 09:58:29 -0500

    use data url

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,20 @@
 var mysql = require("mysql");
 // require("dotenv").config();
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  database: "pizza_db"
+var connection = mysql.createConnection(
+  process.env.DATABASE_URL
+  // {
+  // port: 3306,
+  // host: "localhost",
+  // user: "root",
+  // password: "root",
+  // host: process.env.DATABASE_URL,
+  // user: process.env.DATABASE_URL,
+  // password: process.env.DATABASE_URL,
+  // database: "pizza_db"
   // socketPath: "/var/run/mysqld/mysqld.sock"
-});
+// }
+);
 
 // Make connection.
 connection.connect(function(err) {
